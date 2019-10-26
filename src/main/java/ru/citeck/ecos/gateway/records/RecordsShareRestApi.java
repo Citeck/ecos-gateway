1a410b844e0dfd78fbba5f77ccf5d3efae768276
Pavel Simonov <Sipaha@bk.ru>
2019-10-26 10:31:45 +0700

    Add ability to redirect records query

--- src/main/java/ru/citeck/ecos/gateway/records/RecordsShareRestApi.java
+++ src/main/java/ru/citeck/ecos/gateway/records/RecordsShareRestApi.java
@@ -8,31 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.citeck.ecos.records2.request.rest.DeletionBody;
 import ru.citeck.ecos.records2.request.rest.MutationBody;
 import ru.citeck.ecos.records2.request.rest.QueryBody;
-import ru.citeck.ecos.records2.request.rest.RestHandler;
+import ru.citeck.ecos.records2.spring.RecordsRestApi;
 
 @RestController
 @RequestMapping("/share/api/records")
 public class RecordsShareRestApi {
 
-    private RestHandler restHandler;
+    private RecordsRestApi restApi;
 
     @Autowired
-    public RecordsShareRestApi(RestHandler restHandler) {
-        this.restHandler = restHandler;
+    public RecordsShareRestApi(RecordsRestApi restApi) {
+        this.restApi = restApi;
     }
 
     @PostMapping("/query")
     public Object recordsQuery(@RequestBody QueryBody body) {
-        return restHandler.queryRecords(body);
+        return restApi.recordsQuery(body);
     }
 
     @PostMapping("/mutate")
     public Object recordsMutate(@RequestBody MutationBody body) {
-        return restHandler.mutateRecords(body);
+        return restApi.recordsMutate(body);
     }
 
     @PostMapping("/delete")
     public Object recordsDelete(@RequestBody DeletionBody body) {
-        return restHandler.deleteRecords(body);
+        return restApi.recordsDelete(body);
     }
 }
