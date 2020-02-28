1e873a795c6a7de03893e963bf59cf6f4c172bc0
Pavel Simonov <Sipaha@bk.ru>
2020-02-28 13:28:12 +0700

    Release 1.7.1

--- src/main/java/ru/citeck/ecos/gateway/records/RecordsShareRestApi.java
+++ src/main/java/ru/citeck/ecos/gateway/records/RecordsShareRestApi.java
@@ -1,13 +1,11 @@
 package ru.citeck.ecos.gateway.records;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.citeck.ecos.records2.request.rest.DeletionBody;
-import ru.citeck.ecos.records2.request.rest.MutationBody;
-import ru.citeck.ecos.records2.request.rest.QueryBody;
 import ru.citeck.ecos.records2.spring.web.rest.RecordsRestApi;
 
 @RestController
@@ -21,17 +19,17 @@ public class RecordsShareRestApi {
         this.restApi = restApi;
     }
 
-    @PostMapping("/query")
+    @PostMapping(value = "/query", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Object recordsQuery(@RequestBody byte[] body) {
         return restApi.recordsQuery(body);
     }
 
-    @PostMapping("/mutate")
+    @PostMapping("/mutate", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Object recordsMutate(@RequestBody byte[] body) {
         return restApi.recordsMutate(body);
     }
 
-    @PostMapping("/delete")
+    @PostMapping("/delete", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Object recordsDelete(@RequestBody byte[] body) {
         return restApi.recordsDelete(body);
     }
